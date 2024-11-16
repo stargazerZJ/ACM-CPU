160c2ec93573453f1ed9312bf0158d4b03232b21
stargazerZJ <53366576+stargazerZJ@users.noreply.github.com>
2024-11-16 17:21:39 +0800

    test: write output to file

--- src/hci.v
+++ src/hci.v
@@ -217,6 +217,11 @@ always @*
       end
   end
 
+integer output_file;
+initial begin
+  output_file = $fopen("test.out", "w");
+end
+
 always @*
   begin
     // Setup default FF updates.
@@ -249,8 +254,7 @@ always @*
               d_tx_data = io_din;
               d_wr_en   = 1'b1;
             end
-            // $display("IO:out:%x",io_din);
-            $write("%c", io_din);
+            $fwrite(output_file, "%c", io_din);
           end
           3'h4: begin      // 0x30004 write: indicates program stop
             if (!tx_full) begin
